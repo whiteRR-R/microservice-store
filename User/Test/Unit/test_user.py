55c09e394f81f67ade9e1356d78b15714e6e09c6
whiteRR <whiterr2134@gmail.com>
2024-08-26 16:31:59 +0500

    created usertestcase

---
+++ User/Test/Unit/test_user.py
@@ -0,0 +1,14 @@
+from Domain.Entities.user import User
+
+def test_create_user():
+    user = User(
+        username="alex_m",
+        firstname="Alex",
+        lastname="Mersers",
+        email="alex@gmail.com",
+        password="password1234"
+    )
+    
+    assert user.firstname == "Alex"
+
+    
