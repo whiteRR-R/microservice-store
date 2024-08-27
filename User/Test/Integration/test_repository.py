0eae256b7e1a69b172c8cdfa3c4c5ef9b9a81109
whiteRR <whiterr2134@gmail.com>
2024-08-27 13:02:32 +0500

    created fake repository test

---
+++ User/Test/Integration/test_repository.py
@@ -0,0 +1,39 @@
+from Repositories.fake_user_repository import FakeUserRepository
+from Domain.Entities.user import User
+
+
+def create_user(
+    username: str,
+    firstname: str,
+    lastname: str,
+    email: str,
+    password: str,
+):
+    return User(username, firstname, lastname, email, password)
+
+
+def test_user_created():
+    repo = FakeUserRepository()
+    user = create_user(
+        "alex_m",
+        "alex",
+        "mercer",
+        "user@gmail.com",
+        "test"
+    )
+    
+    repo.add(user)
+
+    users_list = repo.list()
+    
+    assert user in users_list 
+
+
+    
+    
+    
+    
+    
+    
+    
+    
