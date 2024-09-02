1dbd8893bae4a75a9b7bad34854ce2c4dfb1055a
whiteRR <whiterr2134@gmail.com>
2024-09-02 17:16:28 +0500

    changed database url

--- User/Infrastructure/Persistence/database.py
+++ User/Infrastructure/Persistence/database.py
@@ -5,42 +5,39 @@ from sqlalchemy.ext.asyncio import (
     async_sessionmaker,
 )
 from sqlalchemy.ext.declarative import declarative_base
-
+from config import config
 Base = declarative_base()
 
 
 class AsyncSqlAlchemyDatabase:
     _isinstance = None
-    
+
     def __new__(cls, *args, **kwargs):
         if not cls._isinstance:
             cls._isinstance = super(AsyncSqlAlchemyDatabase, cls).__new__(cls, *args, **kwargs)
             cls._isinstance._engine = None
             cls._isinstance.session_maker = None
         return cls._isinstance
-    
+
     def initialize(self, db_url: str):
         if not self._engine:
-            self._engine = create_async_engine(url=db_url, echo=True)
+            self._engine = create_async_engine(url=config.database.database_url, echo=True)
             self._session_maker = async_sessionmaker(
-                self._engine,
-                expire_on_commit=False,
-                class_=AsyncSession
+                self._engine, expire_on_commit=False, class_=AsyncSession
             )
-    
+
     @property
     def engine(self) -> AsyncSession:
         if not self._engine:
             raise ValueError("Database not initialized. Call 'initialize' first.")
         return self._engine
-                
+
     @property
     def session(self) -> AsyncSession:
         if not self._session_maker:
             raise ValueError("Database not initialized. Call 'initialize' first.")
-        return self._session_maker()   
-    
-    
+        return self._session_maker()
+
 
 async def get_session() -> AsyncSession:
     return AsyncSqlAlchemyDatabase().session
