1720f0581365d4bfcf20f46b4d707397673109fa
whiteRR <whiterr2134@gmail.com>
2024-09-03 18:02:53 +0500

    changed the database logic

--- User/Infrastructure/Persistence/database.py
+++ User/Infrastructure/Persistence/database.py
@@ -1,43 +1,38 @@
 from sqlalchemy.ext.asyncio import (
     AsyncSession,
-    AsyncEngine,
     create_async_engine,
     async_sessionmaker,
 )
 from sqlalchemy.ext.declarative import declarative_base
+from contextlib import asynccontextmanager
 from config import config
+
 Base = declarative_base()
 
 
-class AsyncSqlAlchemyDatabase:
+class DataBase:
     _isinstance = None
 
     def __new__(cls, *args, **kwargs):
         if not cls._isinstance:
-            cls._isinstance = super(AsyncSqlAlchemyDatabase, cls).__new__(cls, *args, **kwargs)
-            cls._isinstance._engine = None
-            cls._isinstance.session_maker = None
+            cls._isinstance = super(DataBase, cls).__new__(cls, *args, **kwargs)
+            cls._instance._engine = None
+            cls._instance._session_maker = None
         return cls._isinstance
 
-    def initialize(self, db_url: str):
+    async def __init__(self, db_url: str):
         if not self._engine:
-            self._engine = create_async_engine(url=config.database.database_url, echo=True)
+            self._engine = create_async_engine(url=db_url, echo=True)
             self._session_maker = async_sessionmaker(
                 self._engine, expire_on_commit=False, class_=AsyncSession
             )
 
-    @property
-    def engine(self) -> AsyncSession:
-        if not self._engine:
-            raise ValueError("Database not initialized. Call 'initialize' first.")
-        return self._engine
-
-    @property
-    def session(self) -> AsyncSession:
-        if not self._session_maker:
-            raise ValueError("Database not initialized. Call 'initialize' first.")
-        return self._session_maker()
-
-
-async def get_session() -> AsyncSession:
-    return AsyncSqlAlchemyDatabase().session
+    @asynccontextmanager
+    async def get_session(self):
+        session: AsyncSession = self._session_maker()
+        try:
+            yield session
+        except Exception:
+            await session.rollback()
+        finally:
+            await session.close()
