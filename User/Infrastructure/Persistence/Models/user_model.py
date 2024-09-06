ec695d27d395fe4dd5753b32c107a62dd5d49b6a
whiteRR <whiterr2134@gmail.com>
2024-09-06 16:49:32 +0500

    fixed bugs

--- User/Infrastructure/Persistence/Models/user_model.py
+++ User/Infrastructure/Persistence/Models/user_model.py
@@ -1,18 +1,18 @@
 from sqlalchemy import Integer, String, Boolean, DateTime
-from sqlalchemy.orm import mapped_column
-from database import Base
+from sqlalchemy.orm import mapped_column, Mapped
+from Infrastructure.Persistence.database import Base
 from datetime import datetime
 
 
 class UserModel(Base):
     __tablename__ = "User"
 
-    id: str = mapped_column(Integer, primary_key=True)
-    username = mapped_column(String, unique=True)
-    firstname = mapped_column(String, nullable=False)
-    lastname = mapped_column(String, nullable=False)
-    email = mapped_column(String, nullable=False)
-    password = mapped_column(String, nullable=False)
-    created_at = mapped_column(DateTime, default=datetime)
-    is_active = mapped_column(Boolean, default=True)
-    is_superuser = mapped_column(Boolean, default=False)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    username: Mapped[str] = mapped_column(String, unique=True)
+    firstname: Mapped[str] = mapped_column(String, nullable=False)
+    lastname: Mapped[str] = mapped_column(String, nullable=False)
+    email: Mapped[str] = mapped_column(String, nullable=False)
+    password: Mapped[str] = mapped_column(String, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True)
+    is_superuser: Mapped[bool] = mapped_column(Boolean, default=False)
