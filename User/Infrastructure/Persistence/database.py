4284c707303a1ffe66422fb2cc073249bb7ae6ee
whiteRR <whiterr2134@gmail.com>
2024-09-09 17:37:11 +0500

    created create_database method

--- User/Infrastructure/Persistence/database.py
+++ User/Infrastructure/Persistence/database.py
@@ -29,18 +29,17 @@ class DataBase:
                 self._engine, expire_on_commit=False, class_=AsyncSession
             )
 
+    async def create_database(self):
+        async with self._engine.begin() as conn:
+            await conn.run_sync(Base.metadata.create_all)
+    
     @asynccontextmanager
     async def get_session(self):
         session: AsyncSession = self._session_maker()
 
-        async with self._engine.begin() as conn:
-            await conn.run_sync(Base.metadata.create_all)
         try:
             yield session
         except Exception:
             await session.rollback()
         finally:
             await session.close()
-
-
-db = DataBase(config.database.database_url)
