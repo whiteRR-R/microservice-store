625552c282c8dd0905ed5e375f6adcf90a3d0e97
whiteRR <whiterr2134@gmail.com>
2024-09-12 13:00:45 +0500

    changed user scheme

--- User/Presentation/Scheme/user.py
+++ User/Presentation/Scheme/user.py
@@ -1,10 +1,10 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
 class UserScheme(BaseModel):
     username: str
     firstname: str
     lastname: str
-    email: str
+    email: EmailStr
     password: str
 
     
