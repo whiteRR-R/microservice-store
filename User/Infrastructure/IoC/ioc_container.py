af0ae7f62b6d813d96b1c6e42e42cdcdd7861724
whiteRR <whiterr2134@gmail.com>
2024-09-19 14:48:26 +0500

    changed the storage path of the ioc container

--- User/Infrastructure/IoC/ioc_container.py
+++ User/Infrastructure/IoC/ioc_container.py
@@ -1,5 +1,4 @@
 from typing import Type, Any
-from Applicatation.Services.user import UserService
 from Infrastructure.Persistence.database import DataBase
 from Infrastructure.Persistence.Repositories.sqlalchemy_repository_impl import SQLAlchemyUserRepository
 from Infrastructure.Persistence.UoW.sqlalchemy_uow import SQLAlchemyUnitOfWork
@@ -9,10 +8,11 @@ from Infrastructure.config import config
 class IoCContainer:
     def __init__(self) -> None:
         self._container: dict [Type, Any] = {}
+        self.session = None
     
-    def register(self, type, service):
+    def register(self, type, callable):
         if type not in self._container:
-            self._container[type] = service
+            self._container[type] = callable
     
     def get(self, type):
         return self._container[type] if type in self._container else None
@@ -22,13 +22,6 @@ class IoCContainer:
 
         async with database.get_session() as session:
             sqlalchemy_uow = SQLAlchemyUnitOfWork(session)
-            user_service = UserService(sqlalchemy_uow)
-            self.register(UserService, user_service)
+            self.register("SQLAlchemyUnitOfWork", sqlalchemy_uow)
 
-
-async def get_ioc_container() -> IoCContainer:
-    ioc = IoCContainer()
-    await ioc.initializate()
-    print(ioc._container)
-
-    return ioc
+ 
