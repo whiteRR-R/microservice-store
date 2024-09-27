d9a94cd0cb3abe12cbecbe339f8aa9ce857b004d
whiteRR <whiterr2134@gmail.com>
2024-09-27 09:41:43 +0500

    reinstalling alembic

--- User/Infrastructure/Persistence/database.py
+++ User/Infrastructure/Persistence/database.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.asyncio import (
 )
 from sqlalchemy.orm import DeclarativeBase
 from contextlib import asynccontextmanager
-from Infrastructure.config import config
+from Infrastructure.config import config_manager
 
 
 class Base(DeclarativeBase):
