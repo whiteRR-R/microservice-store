9fffdfd76fd079488562eb116855668d63b71fd8
whiteRR <whiterr2134@gmail.com>
2024-09-28 17:02:46 +0500

    created a model for an auth user

---
+++ Auth/Infrastructure/Models/user_model.py
@@ -0,0 +1,12 @@
+from Infrastructure.database import Base
+from sqlalchemy import String, Integer
+from sqlalchemy.orm import mapped_column, Mapped
+
+
+class UserModel(Base):
+    __tablename__ = "user"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    username: Mapped[str] = mapped_column(String, unique=True)
+    email: Mapped[str] = mapped_column(String, nullable=False)
+    password: Mapped[str] = mapped_column(String, nullable=False)
