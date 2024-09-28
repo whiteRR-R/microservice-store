0b146c0672fa682e5306ab400b70941b98e8b36f
whiteRR <whiterr2134@gmail.com>
2024-09-28 15:48:32 +0500

    Alembic connection settings installed

--- User/Infrastructure/Migrations/env.py
+++ User/Infrastructure/Migrations/env.py
@@ -18,10 +18,10 @@ if config.config_file_name is not None:
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = None
-
+from Infrastructure.Persistence.database import Base
+from Infrastructure.config import config_manager
+target_metadata = Base.metadata
+config.set_main_option("sqlalchemy.url", config_manager.database.database_url)
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
