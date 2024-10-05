b8f6251bc1a04569b753425cc78c76ec3f29adba
whiteRR <whiterr2134@gmail.com>
2024-10-05 15:36:26 +0500

    added jwt settings

--- Auth/config.py
+++ Auth/config.py
@@ -1,7 +1,15 @@
 import os
 from dotenv import load_dotenv
+from pathlib import Path
 
+BASE_DIR = Path(__file__).resolve().parent
 load_dotenv()
+
+
+class JWTSettings:
+    private_key: Path = BASE_DIR / "Infrastructure" / "Security" / "certs" / "private-key.pem"
+    public_key: Path = BASE_DIR / "Infrastructure" / "Security" / "certs" / "public-key.pem"
+    algorithm: str = "RS256"
 
 
 class DatabaseSettings:
@@ -10,6 +18,7 @@ class DatabaseSettings:
 
 class ConfigManageSettings:
     database: DatabaseSettings = DatabaseSettings()
+    jwt: JWTSettings = JWTSettings()
 
 
 config_manager = ConfigManageSettings()
