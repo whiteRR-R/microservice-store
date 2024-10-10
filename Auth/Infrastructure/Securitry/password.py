e11b5ade051eb3b4746d0253d1db255b0b912004
whiteRR <whiterr2134@gmail.com>
2024-10-10 12:57:17 +0500

    fixed bugs

--- Auth/Infrastructure/Securitry/password.py
+++ Auth/Infrastructure/Securitry/password.py
@@ -3,13 +3,13 @@ import bcrypt
 
 class PasswordSecurity:
     
-    def get_hash_password(self, password: str):
+    def get_hash_password(self, password: str | bytes):
         salt = bcrypt.gensalt()
         hashed_password = bcrypt.hashpw(password=bytes(password), salt=salt)
 
         return hashed_password
     
-    def verify_password(self, password: str, hashed_password: bytes):
+    def verify_password(self, password: str | bytes, hashed_password: bytes):
         return bcrypt.checkpw(password=password, hashed_password=hashed_password)
     
 
