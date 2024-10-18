9a620f487b07231b71ce8deff14dd842a6743185
whiteRR <whiterr2134@gmail.com>
2024-10-18 13:49:09 +0500

    added not exist exception

--- Auth/Domain/Exceptions/exist.py
+++ Auth/Domain/Exceptions/exist.py
@@ -2,3 +2,9 @@ class ExistException(Exception):
     def __init__(self, message: str):
         self.message = message
         super().__init__(message)
+
+
+class NotExistException(Exception):
+    def __init__(self, message: str):
+        self.message = message
+        super().__init__(message)
