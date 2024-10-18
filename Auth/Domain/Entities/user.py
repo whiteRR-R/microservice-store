476dcda3e2b38ef508a4174ccef0e92375ba5cd1
whiteRR <whiterr2134@gmail.com>
2024-10-18 13:50:54 +0500

    addded update email and update password method

--- Auth/Domain/Entities/user.py
+++ Auth/Domain/Entities/user.py
@@ -1,8 +1,17 @@
+from Exceptions.invalid_data import InvalidDataException
+
 class User:
     def __init__(self, username: str, email: str, password:str):
-        self.username = username
+        self._username = username
         self._email = email
         self._password = password
 
     def __repr__(self) -> str:
         return self.username
+
+    def update_email(self, new_email: str):
+        self._email = new_email
+    
+    def update_password(self, new_password: str | bytes):
+        self._password = new_password
+    
