485e4383ddc10f8b5e97000cb1c995e9c4870690
whiteRR <whiterr2134@gmail.com>
2024-11-27 15:52:48 +0500

    Added implementation for creating a user

---
+++ Auth/Application/Command/create_user_handler.py
@@ -0,0 +1,14 @@
+from Application.Command.create_user_command import CreateUserCommand
+from Application.Service.auth import AuthService
+
+
+class CreateUserCommandHandler:
+    def __init__(self, auth_service: AuthService):
+        self.auth_service = auth_service
+    
+    async def handler(self, command: CreateUserCommand):
+        return await self.auth_service.create_user(
+            username=command.username,
+            password=command.password,
+            email=command.email,
+        )
