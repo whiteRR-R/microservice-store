8517dd397b854c29d61fcf00ae938882eb4f0999
whiteRR <whiterr2134@gmail.com>
2024-11-27 17:39:01 +0500

    dataclass decorator added

--- Auth/Application/Command/delete_user_command.py
+++ Auth/Application/Command/delete_user_command.py
@@ -1,5 +1,7 @@
 from Application.Command.command import Command
+from dataclasses import dataclass
 
 
+@dataclass
 class DeleteUserCommand(Command):
     username: str
--- Auth/Application/Command/update_email_command.py
+++ Auth/Application/Command/update_email_command.py
@@ -1,6 +1,8 @@
 from Application.Command.command import Command
+from dataclasses import dataclass
 
 
+@dataclass
 class UpdateUserEmailCommand(Command):
     email: str
     new_email: str
