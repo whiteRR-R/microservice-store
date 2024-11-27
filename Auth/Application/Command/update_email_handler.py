d15f5d176c8023bf00323caac621347a2cb0fe10
whiteRR <whiterr2134@gmail.com>
2024-11-27 15:58:02 +0500

    Added implementation for updating a user email

---
+++ Auth/Application/Command/update_email_handler.py
@@ -0,0 +1,13 @@
+from Application.Command.update_email_command import UpdateUserEmailCommand
+from Application.Service.auth import AuthService
+
+
+class UpdateEmailCommandHandler:
+    def __init__(self, auth_service: AuthService):
+        self.auth_service = auth_service
+    
+    async def handler(self, command: UpdateUserEmailCommand):
+        return await self.auth_service.update_email(
+            email=command.email,
+            new_email=command.new_email,
+        )
